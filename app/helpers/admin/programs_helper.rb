b2bbec015c4eceb4abf8f46c1b8205d6eeac8a65
Gene <gorange310@gmail.com>
2022-02-11 10:48:28 +0800

    CRUD program

--- app/helpers/admin/programs_helper.rb
+++ app/helpers/admin/programs_helper.rb
@@ -1,2 +1,13 @@
 module Admin::ProgramsHelper
+  def program_category_selection
+    ProgramCategory.select(:name, :id).map{ |category| [category.name, category.id] }
+  end
+
+  def currency_selection
+    Currency.select(:name, :id).map{ |currency| [currency.name, currency.id] }
+  end
+
+  def program_type_selection
+  ProgramType.select(:name, :id).map{ |type| [type.name, type.id] }
+  end
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,2 +1,13 @@
 class Program < ApplicationRecord
+  STATUS = {
+    :on => '上架',
+    :off => '下架'
+  }
+
+  belongs_to :category, :class_name => 'ProgramCategory', :foreign_key => 'program_category_id'
+  belongs_to :type, :class_name => 'ProgramType', :foreign_key => 'program_type_id'
+  belongs_to :currency
+
+  validates :status, :presence => true
+  validates :validity_period, :presence => true
 end
